dbd6d0dbb1b60be90348e5db5bf96baf2880301c
mirageN1349 <desertirpipi@gmail.com>
2020-07-11 16:26:50 +0300

    slider create

--- src/slider/slider.js
+++ src/slider/slider.js
@@ -1,11 +1,64 @@
-const navBtn = document.querySelectorAll('.slider-button-nav')
-navBtn[0].classList.add('active-nav')
+const navBtn = document.querySelectorAll('.slider-button-nav'),
+  navRight = document.querySelector('.slider-button-right'),
+  navLeft = document.querySelector('.slider-button-left'),
+  slides = document.querySelectorAll('.slide'),
+  carousel = document.querySelector('.slider-carousel')
 
-navBtn.forEach(btn => {
+let counter = 0
+
+navRight.addEventListener('click', nextSlide(true))
+navLeft.addEventListener('click', prevSlide)
+
+navBtn.forEach((btn, index) => {
   btn.addEventListener('click', () => {
-    navBtn.forEach(btn => {
-      btn.classList.remove('active-nav')
-    })
-    btn.classList.toggle('active-nav')
+    currentSlide(index)
+    counter = index
+    clearInterval(interval)
   })
 })
+
+const interval = setInterval(nextSlide(false), 3000)
+
+function activeSlide(index) {
+  slides.forEach(slide => {
+    slide.classList.remove('animate__fadeOutRight')
+    slide.classList.remove('active')
+  })
+  slides[index].classList.add('active')
+  slides[index].classList.add('animate__zoomIn')
+}
+
+function activeNavBtn(index) {
+  navBtn.forEach(btn => {
+    btn.classList.remove('active')
+  })
+  navBtn[index].classList.add('active')
+}
+
+function currentSlide(index) {
+  activeSlide(index)
+  activeNavBtn(index)
+}
+
+function nextSlide(check = true) {
+  return function () {
+    check ? clearInterval(interval) : false
+    if (counter === slides.length - 1) {
+      counter = 0
+      currentSlide(counter)
+    } else {
+      counter++
+      currentSlide(counter)
+    }
+  }
+}
+
+function prevSlide() {
+  if (counter === 0) {
+    counter = slides.length - 1
+    currentSlide(counter)
+  } else {
+    counter--
+    currentSlide(counter)
+  }
+}
