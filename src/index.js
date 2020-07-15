fcddb314b7e88c5a405156da4e264740940b47fe
mirageN1349 <desertirpipi@gmail.com>
2020-07-16 01:16:17 +0300

    add fake like

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './scss/index.scss'
 import './button/m-400'
 import './slider/slider'
+import { localStorageHelper } from './core/localStorage'
 
+const storage = localStorageHelper()
+
+const likeText = document.querySelector('.program-like span')
 const btnMore = document.querySelector('.program-description__button')
 const descriptionText = document.querySelector('.program-description__text')
 const blur = document.querySelector('.program-description__blur')
 const like = document.querySelector('.program-like')
-const likeText = document.querySelector('.program-like span')
+
+likeText.innerHTML = storage.getItem('likeText') || 120
+checkLikeState(121)
+
+storage.setItem('likeText', likeText.innerHTML)
 
 btnMore.addEventListener('click', btnMoreClick)
 like.addEventListener('click', btnLikeClick)
@@ -16,7 +24,18 @@ function btnMoreClick() {
   descriptionText.classList.toggle('active-btn')
 }
 
+function checkLikeState(state) {
+  likeText.innerHTML == state
+    ? like.classList.add('active')
+    : like.classList.remove('active')
+}
+
 function btnLikeClick() {
-  like.classList.toggle('active')
-  likeText.textContext
+  checkLikeState(120)
+  let counter = storage.getItem('likeText')
+  counter === 120
+    ? storage.setItem('likeText', counter + 1)
+    : storage.setItem('likeText', counter - 1)
+
+  likeText.innerHTML = storage.getItem('likeText')
 }
